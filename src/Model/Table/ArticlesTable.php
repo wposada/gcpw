1383581c50ca6864655d7ef9041963405aa487cb
wposada <wilsonposada@msn.com>
2015-06-08 18:54:11 -0500

    Create ArticlesTable.php

---
+++ src/Model/Table/ArticlesTable.php
@@ -0,0 +1,12 @@
+<?
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class ArticlesTable extends Table
+{
+    public function initialize(array $config)
+    {
+        $this->addBehavior('Timestamp');
+    }
+}
