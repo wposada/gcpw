635d986710322b85bc14582ca5fa0041269e4d68
wposada <wilsonposada@msn.com>
2015-06-08 19:00:25 -0500

    Update ArticlesController.php

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -3,4 +3,10 @@ namespace App\Controller;
 
 class ArticlesController extends AppController
 {
+
+    public function index()
+    {
+        $articles = $this->Articles->find('all');
+        $this->set(compact('articles'));
+    }
 }
