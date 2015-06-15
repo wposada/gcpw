45ed5400b0388e4b72e0f9fd03666b2e8cdfae45
wposada <wilsonposada@msn.com>
2015-06-15 15:51:34 -0500

    Update PortalsController.php

--- src/Controller/PortalsController.php
+++ src/Controller/PortalsController.php
@@ -15,7 +15,7 @@ class PortalsController extends AppController
 
           // Simple get
         //$response = $http->get('http://cerebro.botnyx.com/a/portal/083a8841b05140dc8dd7dacd0024b265.16');////1portal
-        $response= $http->get('http://cerebro.botnyx.com/a/portals/4.649456/-74.101633/0');
+        $response= $http->get('http://cerebro.botnyx.com/a/portals/4.649456/-74.101633/1');
         $portal = $this->Portals->get($id);
         $me= $response ->json;
         //foreach($me as $key => $value)
