8e6706e9822d1df87381d7bfcf8cc23128dd3e30
wposada <wilsonposada@msn.com>
2015-06-15 21:14:14 -0500

    Update PortalsController.php

--- src/Controller/PortalsController.php
+++ src/Controller/PortalsController.php
@@ -35,7 +35,7 @@ class PortalsController extends AppController
                 
             }
             $response = $http->get('http://cerebro.botnyx.com/a/portal/'.$portalSave->guid);
-            $portalUpdate=json_decode($response->body);
+            $portalUpdate=$response->body;
             echo $portalUpdate;
             $portalsTable->save($portalSave);
         } 
