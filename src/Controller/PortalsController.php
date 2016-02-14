fbf8cdd61c1f4e5483cfb509441e2c12d89f1619
wposada <wilsonposada@msn.com>
2016-02-13 20:49:52 -0500

    Update PortalsController.php

--- src/Controller/PortalsController.php
+++ src/Controller/PortalsController.php
@@ -25,7 +25,7 @@ class PortalsController extends AppController
     {
 
         $portals = TableRegistry::get('Portals');
-        $portal = $portals->get(2);
+        $portal = $portals->get(['id' => "2"]);
         $portal-> agent = "123";
         //$portal-> lat =$lat;
         //$portal-> name =$name;
