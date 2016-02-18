56cfd60712b3b5cd52be8c3aaafb0c255279fc22
wposada <wilsonposada@msn.com>
2016-02-18 12:41:08 -0500

    Update PlayersController.php

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -7,8 +7,7 @@ class PlayersController extends AppController
        public function index($filtering=null)
     {
         $this -> Players -> recursive = 0;
-        $filtering=$this->request['url']['filtering'];
-        //$filtering = $this->request->data('filtering');
+        $filtering=$this->request['url']['filtering']?$this->request->data('filtering');
         $this->set('_f', $filtering);
 	$this -> set("filterPlayer",'');
 	$this->paginate = array('paramType' => 'querystring',
