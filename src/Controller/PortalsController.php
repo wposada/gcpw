962c666184dd35b6ab3e2e63389bfc82f651f87b
wposada <wilsonposada@msn.com>
2016-05-17 11:32:40 -0500

    Update PortalsController.php

--- src/Controller/PortalsController.php
+++ src/Controller/PortalsController.php
@@ -112,10 +112,10 @@ public function addmus($faction=null,$agent=null,$captured=null,$mus=0)
     } 
     public function mus()
     {
-    //	$mus = TableRegistry::get('Mus');
-    //	$query = $mus->find();
-	//$query->select(['sum' => $query->func()->sum('*')]);
-	$query="123";
+    	$mus = TableRegistry::get('Mus');
+    	$query = $mus->find();
+	$query->select(['sum' => $query->func()->sum('*')]);
+	//$query="123";
         $this->set('g', $query);
     } 
     
