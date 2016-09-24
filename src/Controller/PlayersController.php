5429ff422bccd74cb9232456841e2fb3381884f2
wposada <wilsonposada@msn.com>
2016-09-24 11:58:56 -0500

    Update PlayersController.php

--- src/Controller/PlayersController.php
+++ src/Controller/PlayersController.php
@@ -15,7 +15,7 @@ public function getTimeCapture($lng,$lat){
 	$connection = ConnectionManager::get('default');
 	
 	//
-	$result = $connection->execute('SELECT agent,captured FROM `guardians` WHERE `lng` = -74.114352 AND `lat` = 4.602126 ORDER BY `guardians`.`captured` DESC limit 1'->fetchAll('assoc');
+	$result = $connection->execute("SELECT agent,captured FROM `guardians` WHERE `lng` = -74.114352 AND `lat` = 4.602126 ORDER BY `guardians`.`captured` DESC limit 1"->fetchAll('assoc');
 	
 	 $this->RequestHandler->renderAs($this, 'json');
 	$output = array(
