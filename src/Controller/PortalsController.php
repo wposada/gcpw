187d7bd2dd6099a72566937198e4f8ab2bffb556
wposada <wilsonposada@msn.com>
2017-03-27 14:40:54 -0500

    Update PortalsController.php

--- src/Controller/PortalsController.php
+++ src/Controller/PortalsController.php
@@ -103,7 +103,7 @@ public function addmus($faction=null,$agent=null,$captured=null,$mus=0)
     	date_default_timezone_set('America/Bogota');
     	$guar = TableRegistry::get('Guardians');
     	$query = $guar->find();
-	$query->select(['agent','captured'])->limit(1000)->order(['captured' => 'DESC']);
+	$query->select(['agent','captured'])->limit(2000)->order(['captured' => 'DESC']);
 /*	foreach ($query as $row) {
 		
 		$dat=date("Y/m/d H:i:s",$row->captured/1000);
